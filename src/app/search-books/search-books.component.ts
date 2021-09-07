36842695f285fa8ad62d3baa9a55690cc0244bbe
Amjad alkhatib <code_amjad@hotmail.com>
2021-09-08 01:27:50 +0300

    Finish design search-books page

---
+++ src/app/search-books/search-books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-books',
+  templateUrl: './search-books.component.html',
+  styleUrls: ['./search-books.component.css']
+})
+export class SearchBooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
